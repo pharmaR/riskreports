f7f6f3c2cf8c20d974794196168c0f9de30b290e
Lluís Revilla <lluis.revilla_sancho@roche.com>
2024-11-06 23:33:15 +0100

    Add progress to working status

--- R/check.R
+++ R/check.R
@@ -2,10 +2,75 @@
 
 #' Reports check results
 #'
-#' @param x Path to check results
+#' @param path Path to check results folder (aka pkgname.Rcheck).
 #'
 #' @return A report
 #' @export
-check_reporter <- function(x){
+#' @seealso [tools::check_packages_in_dir()]
+#' @examples
+#' # Requires the package to be installed (or be in inst/ folder)
+#' check_reporter("checks/riskmetric.Rcheck")
+#'
+check_reporter <- function(path){
+  log <- file.path(path, "00check.log")
+  lines <- readLines(log)
+}
+
+
+#' Export enviromental variables
+#'
+#' @param show Character vector of environmental variables to show.
+#' @param exclude Character vector of environmental variables to hide.
+#'
+#' @return A character vector with environmental variables.
+#' @export
+#'
+#' @examples
+#' environ_report()
+environ_report <- function(show = NULL, exclude = NULL) {
+  environ <- Sys.getenv()
+
+  keep_names <- include_exclude_from(names(environ), show, exclude)
+
+  if (length(keep_names) == 0) {
+    return(NULL)
+  }
+
+
+  env <- fill_in(environ, keep_names)
+
+  # TODO: See note on truncated variables on Windows
+  env
+}
+
+#' Export options
+#'
+#' @param show Character vector of options to show.
+#' @param exclude Character vector of options variables to hide.
+#'
+#' @return A list with the options
+#' @export
+#'
+#' @examples
+#' options_report()
+options_report <- function(show = NULL, exclude = NULL) {
+  options <- options()
+
+  keep_names <- include_exclude_from(names(options), show, exclude)
+
+  if (length(keep_names) == 0) {
+    return(NULL)
+  }
+  fill_in(options, keep_names)
+}
+
+check_summary <- function(path) {
+  out <- tools::check_packages_in_dir_details(path)
+  summary(out)
+}
+
+check_packages <- function(path) {
+
+  check_packages_in_dir_details(path)
 
 }
