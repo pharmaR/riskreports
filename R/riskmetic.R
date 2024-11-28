9e59c748ae5827acc99ed636e84c9267fff04404
Lluís Revilla <185338939+llrs-roche@users.noreply.github.com>
2024-11-28 09:47:22 +0100

    Helpers for riskmetric (#5)
    * Add some work on riskmetric
    
    * Improve error handling

---
+++ R/riskmetic.R
@@ -0,0 +1,48 @@
+
+
+#' Create a
+#'
+#' Vectorize assessment to be able to add it to a report for a package or the PACKAGES file.
+#'
+#' @param assessment An object created by `pkg_assess()` (`list_of_pkg_metric`)
+#' @return
+#' @export
+#'
+#' @examples
+assessment <- function(assessment) {
+  stopifnot("Input is not from riskmetric::pkg_assess()"= is(assessment, "list_of_pkg_metric"))
+
+  len <- lengths(assessment)
+
+  out <- as.list(assessment)
+
+
+  out[["covr_coverage"]] <- out[["covr_coverage"]][["totalcoverage"]]
+  out[["exported_namespace"]] <- risk_error(out[["exported_namespace"]], length(assessment[["exported_namespace"]]))
+  out[["export_help"]] <- risk_error(out[["export_help"]], length(assessment[["export_help"]]))
+  out[["has_website"]] <- risk_error(out[["has_website"]], as.logical(length(assessment[["has_website"]])))
+  out[["bugs_status"]] <- risk_error(out[["bugs_status"]], sum(assessment[["bugs_status"]])/length(assessment[["bugs_status"]]))
+  out[["reverse_dependencies"]] <- risk_error(out[["reverse_dependencies"]], length(assessment[["reverse_dependencies"]]))
+  out[["has_examples"]] <- risk_error(out[["has_examples"]], sum(assessment[["has_examples"]])/length(assessment[["has_examples"]]))
+  out[["dependencies"]] <- risk_error(out[["dependencies"]], nrow(assessment[["dependencies"]]))
+  l <- lengths(out)
+
+  if (any(l) > 1) {
+    out[l > 1] <- lapply(l[l > 1], risk_error)
+  }
+
+  if (any(lengths(out)>0)) {
+    big <- names(out)[lengths(out)>0]
+    warning(paste(big, collapse = ", "), ": cannot be summarized yet")
+    out <- out[lengths(out) == 1]
+  }
+  # browser()
+  list2DF(out)
+}
+
+risk_error <- function(output, y) {
+  if (is(output, "pkg_metric_error")) {
+    return(output$message)
+  }
+  y
+}
