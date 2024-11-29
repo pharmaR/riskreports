1a45dc53d0d3a2f4ec8a896fac11d901b364b2e1
Lluís Revilla <llrs@users.noreply.github.com>
2024-11-29 09:22:29 +0100

    Creat a report (#4)
    * Add template from thevalidatoR: https://github.com/insightsengineering/thevalidatoR/blob/main/template.qmd
    
    * Add packages for the report
    
    * Add function to make the report
    
    * ignore Rstudio project file
    
    * Minimize template dependencies
    
    * Add check parameters
    
    * Add repository to the report
    
    * reuse the coverage from riskmetric's package
    
    * Add GitHub links to DESCRIPTION
    
    * Update report
    
    * Add documentation pages
    
    ---------
    
    Co-authored-by: Lluís Revilla <lluis.revilla_sancho@roche.com>

--- R/reporter.R
+++ R/reporter.R
@@ -3,7 +3,20 @@
 #' @param x Package name or path
 #'
 #' @return A report
+#' @examples
+#' package_report("inst/source/dplyr_1.1.4/dplyr")
+#'
+#' 
 #' @export
 package_report <- function(x){
-
+    package <- basename(x)
+    desc <- read.dcf(file.path(x, "DESCRIPTION"))
+    
+    stopifnot("Mismatch between path and DESCRIPTION name" = package == desc[, "Package"])
+    full_name <- paste0(package, "_v", desc[, "Version"])
+    Sys.setenv("INPUT_REPORT_PKG_DIR" = x)
+    template_path <- system.file("report/template.qmd", package = "riskreports")
+    quarto::quarto_render(template_path, 
+                          output_format = "html",
+                          output_file = paste0("validation_report_", full_name,".html"))
 }
