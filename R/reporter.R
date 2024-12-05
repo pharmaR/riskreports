2ba22a9e0bb7e52a87363cad17fa5510c23fbb40
Pavel Demin <paveldemin@me.com>
2024-12-05 10:44:02 +0000

    refactor: unify function to rely on the source code

--- R/reporter.R
+++ R/reporter.R
@@ -1,60 +1,48 @@
 #' Reports about a package
 #'
 #' @param x Package name or path
+#' @param template_path Path to a custom quarto template file
+#' @param params A list of execute parameters passed to the template
 #'
 #' @return A report
 #' @examples
-#' package_report("inst/source/dplyr_1.1.4/dplyr")
+#' package_report(
+#'   x = "inst/source/dplyr",
+#'   template_path = "inst/report/template.qmd",
+#'   params = list(
+#'     repo = "inst/repos/ubuntu-22.04/4.4",
+#'     package = "dplyr",
+#'     image = "rhub/ref-image",
+#'     assessment_path = "inst/assessments/dplyr.rds",
+#'   )
+#' )
 #'
 #' 
 #' @export
-package_report <- function(x){
+package_report <- function(
+    x,
+    template_path = NULL,
+    params = list()
+) {
     package <- basename(x)
     desc <- read.dcf(file.path(x, "DESCRIPTION"))
     
     stopifnot("Mismatch between path and DESCRIPTION name" = package == desc[, "Package"])
-    full_name <- paste0(package, "_v", desc[, "Version"])
-    Sys.setenv("INPUT_REPORT_PKG_DIR" = x)
-    template_path <- system.file("report/template.qmd", package = "riskreports")
-    quarto::quarto_render(template_path, 
-                          output_format = "html",
-                          output_file = paste0("validation_report_", full_name,".html"))
-}
 
-#' @param package_name Name of the package
-#' @param package_version Version of the package
-#' @param template_path Path to the quarto template file
-#' @param repository Repository where the package is stored
-#' @param docker_image Docker image used to validate the package
-#' @param assessment_path Path to the assessment file (produced by another script)
-#' @param ... Options passed to `quarto::quarto_render()`
-#' @export
-package_report_gh_action <- function(
-    package_name,
-    package_version,
-    template_path = NULL,
-    repository,
-    docker_image,
-    assessment_path,
-    ...
-) {
+    full_name <- paste0(package, "_v", desc[, "Version"])
+    output_file <- paste0("validation_report_", full_name,".html")
+
+    Sys.setenv("INPUT_REPORT_PKG_DIR" = x)
+
     if (is.null(template_path)) {
         template_path <- system.file("report/template.qmd", package = "riskreports")
     }
 
-    full_name <- paste0(package_name, "_v", package_version)
-    output_file <- paste0("validation_report_", full_name,".html")
-
     quarto::quarto_render(
         template_path, 
         output_format = "html",
-        output_file = output_file,
-        execute_params = list(
-            repo = repository,
-            package = package_name,
-            image = docker_image,
-            assessment_path = assessment_path
-        ),
+        output_file = paste0("validation_report_", full_name,".html"),
+        execute_params = params,
         ...
     )
 
