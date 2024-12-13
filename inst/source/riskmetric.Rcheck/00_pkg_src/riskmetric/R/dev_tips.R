f07d756b31642a074d38fc3da6d70743afbb4242
Lluís Revilla <lluis.revilla_sancho@roche.com>
2024-12-13 10:44:10 +0100

    Remove source code and check output

--- inst/source/riskmetric.Rcheck/00_pkg_src/riskmetric/R/dev_tips.R
+++
@@ -1,9 +0,0 @@
-#' #' Provide tips for improving a metric
-#' #'
-#' #' @param x a pkg_metric object
-#' #' @param ... additional arguments unused
-#' #'
-#' #' @export
-#' dev_tips <- function(x, ...) {
-#'   UseMethod("dev_tips")
-#' }
