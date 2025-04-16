8b3d6bf591ed30b0da1e75b7793d3fd036c38e68
osenan <oriol@praenoscere.com>
2025-04-15 23:06:40 -0300

    Feat: Add theming file to customize html report style
    - Refactor package_report function to copy more files than the quarto template
    - Include custom.scss file with custom style
    - Add function to create card with snapshot test
    - Add custom style for card
    - Style html report to match design
    - Include dark mode

--- R/render_utils.R
+++ R/render_utils.R
@@ -9,13 +9,34 @@
 #' To style the card, assign a custom class and style the card within the quarto theme
 #' @keywords internal
 create_info_card <- function(title, header = NULL, text = NULL, extra_class = NULL) {
-  stopifnot(is.character(title))
   header_div <- if (!is.null(header)) div(header, class = "card-header") else header
   text_div <- if (!is.null(text)) div(text, class = "card-text") else text
   div(
     class = paste("card", extra_class),
     header_div,
-    htmltools::h5(title, class = "card-title"),
+    htmltools::h3(title, class = "card-title"),
     text_div
     )
+}
+
+create_metrics_cards <- function(assessment_info) {
+  div(
+    class = "info-card",
+      div(
+        class = "top-card",
+          create_info_card(
+            title = assessment_info$downloads_1yr,
+            # check for icon shapes here: https://icons.getbootstrap.com/
+            header = htmltools::tagList(htmltools::tags$i(class = "bi bi-download"), htmltools::p("1 year")),
+            text = "Downloads",
+            extra_class = "top-left-card"
+          ),
+        div(
+          class = "top-right-card",
+          create_info_card(title = length(assessment_info$reverse_dependencies), header = "Reverse Dependencies", extra_class = "left-card"),
+          create_info_card(title = assessment_info$license, header = "License", text = ">=2", extra_class = "right-card")
+        )
+      ),
+      create_info_card(title = "To be determined", header = "Origin", extra_class = "bottom-card")
+  )
 }
