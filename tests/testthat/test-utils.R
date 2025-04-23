63ce91a608fd24c0e92d5d8bce57219ef228e183
osenan <oriol@praenoscere.com>
2025-04-23 15:36:37 -0300

    refactor: simplify functions, improve titles and include parameter to reflect source in report

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,3 +4,16 @@ test_that("include_exclude_from works", {
   expect_equal(include_exclude_from(from = from, exclude = "a"), "b")
   expect_equal(include_exclude_from(from = from, include = "c"), "c")
 })
+
+test_that("All riskmetric:::pkg_ref_class_hierarchy have value to display in our report", {
+  allowed_names <- unlist(riskmetric:::pkg_ref_class_hierarchy)
+
+  expect_true(all(!is.na(sapply(allowed_names, get_pkg_origin))))
+})
+
+test_that("is_empty returns TRUE if the argument is withint the expected empty categories", {
+  expect_true(is_empty(NULL))
+  expect_true(is_empty(NA))
+  expect_true(is_empty(""))
+  expect_false(is_empty(letters[1]))
+})
